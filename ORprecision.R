5de466274d7d375d9faa18b26fbba2d09c52b2f4
Travis Gerke <tgerke@hsph.harvard.edu>
2017-06-12 21:05:50 -0400

    finish simulation set up. Plot is busted

--- ORprecision.R
+++ ORprecision.R
@@ -43,38 +43,92 @@ summary(unlist(lapply(dats, function(D) {sum(D$x[D$y==0])/sum(D$y==0)})))
 
 iter <- 250
 pControls <- seq(.05,.95,by=.02)
-sim1 <- list()
+sim <- list()
 
+### pCases=.95
 n <- 300
 cacoRatio <- 2
 pCases <- .95
 set.seed(8675309)
 for (k in 1:length(pControls)) {
-   sim1[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
+   sim[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
    for (i in 1:iter) {
       dat <- binSim(n, cacoRatio, pCases, pControls=pControls)
       fit <- glm(y ~ x, family=binomial, data=dat)
       tidyobj <- tidy(fit)[2,]
       tidyCI <- confint_tidy(fit)[2,]
-      sim1[[k]][i,] <- c(exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
+      sim[[k]][i,] <- c(exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
    }
 }
 
-sim1dat <- data.frame(OR=unlist(lapply(sim1, function(x) mean(x$OR))),
-                      lower=unlist(lapply(sim1, function(x) mean(x$lower))),
-                      upper=unlist(lapply(sim1, function(x) mean(x$upper))),
-                      ps=unlist(lapply(sim1, function(x) mean(x$p))),
-                      diffs=diffs)
-sim1dat$upper[which(is.infinite(sim1dat$upper))] <- 1e10
-sim1dat$upper[which(is.na(sim1dat$upper))] <- 1e10
-sim1dat$upper[which(sim1dat$upper>1e10)] <- 1e10 #ggplot2 can't handle very large numbers
+simdat <- data.frame(OR=unlist(lapply(sim, function(x) mean(x$OR))),
+                      lower=unlist(lapply(sim, function(x) mean(x$lower))),
+                      upper=unlist(lapply(sim, function(x) mean(x$upper))),
+                      ps=unlist(lapply(sim, function(x) mean(x$p))),
+                      pControls=pControls, pCases=pCases)
 
-p <- ggplot(sim1dat) + geom_smooth(aes(y=OR, x=diffs), se=FALSE) +
-   geom_ribbon(aes(ymin=lower, ymax=upper, x=diffs, fill = "band"), alpha = 0.3) +
-   xlab("P(Exposed | Case) - P(Exposed | Control)") + ylab("Estimated odds ratio")
-   #facet_wrap(~nGrp) #invokes side-by-side panels, if needed
+### pCases=.75
+n <- 300
+cacoRatio <- 2
+pCases <- .75
+set.seed(8675309)
+for (k in 1:length(pControls)) {
+  sim[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
+  for (i in 1:iter) {
+    dat <- binSim(n, cacoRatio, pCases, pControls=pControls)
+    fit <- glm(y ~ x, family=binomial, data=dat)
+    tidyobj <- tidy(fit)[2,]
+    tidyCI <- confint_tidy(fit)[2,]
+    sim[[k]][i,] <- c(exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
+  }
+}
+
+# bind this simulated data to the previous
+binddat <- data.frame(OR=unlist(lapply(sim, function(x) mean(x$OR))),
+                     lower=unlist(lapply(sim, function(x) mean(x$lower))),
+                     upper=unlist(lapply(sim, function(x) mean(x$upper))),
+                     ps=unlist(lapply(sim, function(x) mean(x$p))),
+                     pControls=pControls, pCases=pCases)
+simdat <- rbind(simdat, binddat)
+
+### pCases=.5
+n <- 300
+cacoRatio <- 2
+pCases <- .5
+set.seed(8675309)
+for (k in 1:length(pControls)) {
+  sim[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
+  for (i in 1:iter) {
+    dat <- binSim(n, cacoRatio, pCases, pControls=pControls)
+    fit <- glm(y ~ x, family=binomial, data=dat)
+    tidyobj <- tidy(fit)[2,]
+    tidyCI <- confint_tidy(fit)[2,]
+    sim[[k]][i,] <- c(exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
+  }
+}
+
+# bind this simulated data to the previous
+binddat <- data.frame(OR=unlist(lapply(sim, function(x) mean(x$OR))),
+                      lower=unlist(lapply(sim, function(x) mean(x$lower))),
+                      upper=unlist(lapply(sim, function(x) mean(x$upper))),
+                      ps=unlist(lapply(sim, function(x) mean(x$p))),
+                      pControls=pControls, pCases=pCases)
+simdat <- rbind(simdat, binddat)
+
+#ggplot2 can't handle infinite or very large numbers
+simdat$upper[which(is.infinite(simdat$upper))] <- 1e10
+simdat$upper[which(is.na(simdat$upper))] <- 1e10
+simdat$upper[which(simdat$upper>1e10)] <- 1e10 
+
+p <- ggplot(simdat, aes(y=OR, x=pControls, colour=as.factor(pCases))) + 
+   geom_smooth(se=FALSE) + 
+   geom_ribbon(aes(ymin=lower, ymax=upper, fill="band"), alpha = 0.3) +
+   xlab("P(Exposed | Control)") + ylab("Estimated odds ratio") +
+   scale_y_continuous(limits=c(0, 5))
+   #coord_cartesian(ylim=c(0, 10)) 
+   #facet_wrap(~pCases) #invokes side-by-side panels
 
 ggplotly(p)
 
 
-plot(diffs, sim1dat$OR)
+plot(diffs, simdat$OR)
