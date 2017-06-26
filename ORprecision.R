dba7d6c7c7d2651fd6ed20b11695ce83b6e6f4e1
Travis Gerke <tgerke@hsph.harvard.edu>
2017-06-26 14:44:20 -0400

    add unexponentiated estimates. correct loop indexing error

--- ORprecision.R
+++ ORprecision.R
@@ -42,6 +42,9 @@ pCases/(1-pCases)/(pControls/(1-pControls))
 summary(unlist(lapply(dats, function(D) {
    exp(coef(glm(y~x,family=binomial, data=D)))[2]})))
 
+# clean up 
+rm(list=setdiff(ls(), "binSim"))
+
 #######################################################################################
 # run simulation
 # plan: fix n, pCases, and cacoRatio in a few reasonable panels; then,
@@ -57,21 +60,29 @@ cacoRatio <- 2
 pCases <- .95
 set.seed(8675309)
 for (k in 1:length(pControls)) {
-   sim[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
+   sim[[k]] <- data.frame(est=rep(0, iter), lower=0, upper=0, 
+                          OR=0, ORlower=0, ORupper=0, p=0)
    for (i in 1:iter) {
-      dat <- binSim(n, cacoRatio, pCases, pControls=pControls)
+      dat <- binSim(n, cacoRatio, pCases, pControls=pControls[k])
       fit <- glm(y ~ x, family=binomial, data=dat)
       tidyobj <- tidy(fit)[2,]
       tidyCI <- confint_tidy(fit)[2,]
-      sim[[k]][i,] <- c(exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
+      sim[[k]][i,] <- c(tidyobj$est, tidyCI,
+         exp(tidyobj$est), exp(tidyCI), tidyobj$p.value)
    }
 }
 
-simdat <- data.frame(OR=unlist(lapply(sim, function(x) mean(x$OR))),
-                      lower=unlist(lapply(sim, function(x) mean(x$lower))),
-                      upper=unlist(lapply(sim, function(x) mean(x$upper))),
+simdat <- data.frame(est=unlist(lapply(sim, function(x) median(x$est))),
+                      lower=unlist(lapply(sim, function(x) median(x$lower))),
+                      upper=unlist(lapply(sim, function(x) median(x$upper))),
+                      OR=unlist(lapply(sim, function(x) median(x$OR))),
+                      ORlower=unlist(lapply(sim, function(x) median(x$ORlower))),
+                      ORupper=unlist(lapply(sim, function(x) median(x$ORupper))),
                       ps=unlist(lapply(sim, function(x) mean(x$p))),
-                      pControls=pControls, pCases=pCases)
+                      pControls=pControls, pCases=pCases, 
+                      ORtrue=pCases/(1-pCases)/(pControls/(1-pControls)))
+
+
 
 ### pCases=.75
 n <- 300
@@ -105,7 +116,7 @@ set.seed(8675309)
 for (k in 1:length(pControls)) {
   sim[[k]] <- data.frame(OR=rep(0, iter), lower=0, upper=0, p=0)
   for (i in 1:iter) {
-    dat <- binSim(n, cacoRatio, pCases, pControls=pControls)
+    dat <- binSim(n, cacoRatio, pCases, pControls=pControls[k])
     fit <- glm(y ~ x, family=binomial, data=dat)
     tidyobj <- tidy(fit)[2,]
     tidyCI <- confint_tidy(fit)[2,]
@@ -126,7 +137,7 @@ simdat$upper[which(is.infinite(simdat$upper))] <- 1e10
 simdat$upper[which(is.na(simdat$upper))] <- 1e10
 simdat$upper[which(simdat$upper>1e10)] <- 1e10 
 
-p <- ggplot(simdat, aes(y=OR, x=pControls, colour=as.factor(pCases))) + 
+ORp <- ggplot(simdat, aes(y=OR, x=pControls, colour=as.factor(pCases))) + 
    geom_smooth(se=FALSE) + 
    geom_ribbon(aes(ymin=lower, ymax=upper, fill="band"), alpha = 0.3) +
    xlab("P(Exposed | Control)") + ylab("Estimated odds ratio") +
@@ -134,7 +145,11 @@ p <- ggplot(simdat, aes(y=OR, x=pControls, colour=as.factor(pCases))) +
    #coord_cartesian(ylim=c(0, 10)) 
    #facet_wrap(~pCases) #invokes side-by-side panels
 
-ggplotly(p)
+ggplotly(ORp)
 
+simdat$ORupper[which(is.na(simdat$ORupper))] <- 1e10
+plot(simdat$pControls, simdat$OR, ylim=range(c(simdat$ORlower, simdat$ORupper)))
+lines(simdat$pControls, simdat$ORlower, lty=2)
+lines(simdat$pControls, simdat$ORupper, lty=2)
 
 plot(diffs, simdat$OR)
